1570b09dd32ebc10b6e12512e698af395c06cd8d
Thibault Cam <thibault.cam1@yahoo.fr>
2016-12-01 23:06:21 +0100

    Last commit ?

--- TheCurseOfTheSurvivalMigrant.GUI/Game.Designer.cs
+++ TheCurseOfTheSurvivalMigrant.GUI/Game.Designer.cs
@@ -30,47 +30,145 @@
         {
             this.panelText = new System.Windows.Forms.Panel();
             this.EnigmaLabel = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.progressBarSocial = new System.Windows.Forms.ProgressBar();
-            this.progressBarMoney = new System.Windows.Forms.ProgressBar();
-            this.progressBarLife = new System.Windows.Forms.ProgressBar();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelAnswer1 = new System.Windows.Forms.Panel();
             this.label6 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.panelAnswer2 = new System.Windows.Forms.Panel();
             this.label7 = new System.Windows.Forms.Label();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.panelAnswer3 = new System.Windows.Forms.Panel();
             this.label8 = new System.Windows.Forms.Label();
             this.menuButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.panelText.SuspendLayout();
+            this.panelAnswer1.SuspendLayout();
+            this.panelAnswer2.SuspendLayout();
+            this.panelAnswer3.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelText
             // 
             this.panelText.BackColor = System.Drawing.Color.DarkOrange;
             this.panelText.Controls.Add(this.EnigmaLabel);
-            this.panelText.Location = new System.Drawing.Point(0, 670);
-            this.panelText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelText.Location = new System.Drawing.Point(0, 544);
             this.panelText.Name = "panelText";
-            this.panelText.Size = new System.Drawing.Size(1440, 217);
+            this.panelText.Size = new System.Drawing.Size(1080, 176);
             this.panelText.TabIndex = 0;
             // 
             // EnigmaLabel
             // 
             this.EnigmaLabel.AutoSize = true;
-            this.EnigmaLabel.Location = new System.Drawing.Point(671, 107);
-            this.EnigmaLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.EnigmaLabel.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnigmaLabel.Location = new System.Drawing.Point(35, 93);
             this.EnigmaLabel.Name = "EnigmaLabel";
-            this.EnigmaLabel.Size = new System.Drawing.Size(45, 17);
+            this.EnigmaLabel.Size = new System.Drawing.Size(57, 22);
             this.EnigmaLabel.TabIndex = 7;
             this.EnigmaLabel.Text = "{Text}";
+            // 
+            // panelAnswer1
+            // 
+            this.panelAnswer1.BackColor = System.Drawing.SystemColors.Info;
+            this.panelAnswer1.Controls.Add(this.label6);
+            this.panelAnswer1.Location = new System.Drawing.Point(30, 469);
+            this.panelAnswer1.Name = "panelAnswer1";
+            this.panelAnswer1.Size = new System.Drawing.Size(275, 102);
+            this.panelAnswer1.TabIndex = 2;
+            this.panelAnswer1.Click += new System.EventHandler(this.panelAnswer1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(119, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "{Answer1}";
+            // 
+            // panelAnswer2
+            // 
+            this.panelAnswer2.BackColor = System.Drawing.SystemColors.Info;
+            this.panelAnswer2.Controls.Add(this.label7);
+            this.panelAnswer2.Location = new System.Drawing.Point(404, 469);
+            this.panelAnswer2.Name = "panelAnswer2";
+            this.panelAnswer2.Size = new System.Drawing.Size(275, 102);
+            this.panelAnswer2.TabIndex = 3;
+            this.panelAnswer2.Click += new System.EventHandler(this.panelAnswer2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(112, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "{Answer2}";
+            // 
+            // panelAnswer3
+            // 
+            this.panelAnswer3.BackColor = System.Drawing.SystemColors.Info;
+            this.panelAnswer3.Controls.Add(this.label8);
+            this.panelAnswer3.Location = new System.Drawing.Point(771, 469);
+            this.panelAnswer3.Name = "panelAnswer3";
+            this.panelAnswer3.Size = new System.Drawing.Size(275, 102);
+            this.panelAnswer3.TabIndex = 3;
+            this.panelAnswer3.Click += new System.EventHandler(this.panelAnswer3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(118, 45);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "{Answer2}";
+            // 
+            // menuButton
+            // 
+            this.menuButton.Location = new System.Drawing.Point(918, 0);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(162, 61);
+            this.menuButton.TabIndex = 4;
+            this.menuButton.Text = "Menu";
+            this.menuButton.UseVisualStyleBackColor = true;
+            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vie";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Social";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(121, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Argent";
             // 
             // panel1
             // 
@@ -79,173 +177,34 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.progressBarSocial);
-            this.panel1.Controls.Add(this.progressBarMoney);
-            this.panel1.Controls.Add(this.progressBarLife);
-            this.panel1.Location = new System.Drawing.Point(507, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Location = new System.Drawing.Point(380, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(351, 135);
+            this.panel1.Size = new System.Drawing.Size(263, 110);
             this.panel1.TabIndex = 1;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(161, 101);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 17);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Argent";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(269, 101);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Social";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(161, 101);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 17);
-            this.label2.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 101);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(28, 17);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Vie";
-            // 
-            // progressBarSocial
-            // 
-            this.progressBarSocial.Location = new System.Drawing.Point(273, 4);
-            this.progressBarSocial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.progressBarSocial.Name = "progressBarSocial";
-            this.progressBarSocial.Size = new System.Drawing.Size(25, 89);
-            this.progressBarSocial.TabIndex = 2;
-            // 
-            // progressBarMoney
-            // 
-            this.progressBarMoney.Location = new System.Drawing.Point(165, 4);
-            this.progressBarMoney.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.progressBarMoney.Name = "progressBarMoney";
-            this.progressBarMoney.Size = new System.Drawing.Size(25, 89);
-            this.progressBarMoney.TabIndex = 1;
-            // 
-            // progressBarLife
-            // 
-            this.progressBarLife.Location = new System.Drawing.Point(60, 4);
-            this.progressBarLife.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.progressBarLife.Name = "progressBarLife";
-            this.progressBarLife.Size = new System.Drawing.Size(25, 89);
-            this.progressBarLife.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.Info;
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Location = new System.Drawing.Point(40, 577);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(367, 126);
-            this.panel2.TabIndex = 2;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(159, 55);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(72, 17);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "{Answer1}";
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.SystemColors.Info;
-            this.panel3.Controls.Add(this.label7);
-            this.panel3.Location = new System.Drawing.Point(539, 577);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(367, 126);
-            this.panel3.TabIndex = 3;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(149, 55);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(72, 17);
-            this.label7.TabIndex = 9;
-            this.label7.Text = "{Answer2}";
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.SystemColors.Info;
-            this.panel4.Controls.Add(this.label8);
-            this.panel4.Location = new System.Drawing.Point(1028, 577);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(367, 126);
-            this.panel4.TabIndex = 3;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(157, 55);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(72, 17);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "{Answer2}";
-            // 
-            // menuButton
-            // 
-            this.menuButton.Location = new System.Drawing.Point(1224, 0);
-            this.menuButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.menuButton.Name = "menuButton";
-            this.menuButton.Size = new System.Drawing.Size(216, 75);
-            this.menuButton.TabIndex = 4;
-            this.menuButton.Text = "Menu";
-            this.menuButton.UseVisualStyleBackColor = true;
-            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
             // 
             // Game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.Controls.Add(this.menuButton);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelAnswer3);
+            this.Controls.Add(this.panelAnswer2);
+            this.Controls.Add(this.panelAnswer1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelText);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Game";
-            this.Size = new System.Drawing.Size(1440, 886);
+            this.Size = new System.Drawing.Size(1080, 720);
             this.panelText.ResumeLayout(false);
             this.panelText.PerformLayout();
+            this.panelAnswer1.ResumeLayout(false);
+            this.panelAnswer1.PerformLayout();
+            this.panelAnswer2.ResumeLayout(false);
+            this.panelAnswer2.PerformLayout();
+            this.panelAnswer3.ResumeLayout(false);
+            this.panelAnswer3.PerformLayout();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -253,21 +212,18 @@
         #endregion
 
         private System.Windows.Forms.Panel panelText;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.ProgressBar progressBarSocial;
-        private System.Windows.Forms.ProgressBar progressBarMoney;
-        private System.Windows.Forms.ProgressBar progressBarLife;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label EnigmaLabel;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelAnswer1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelAnswer2;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panelAnswer3;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button menuButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- TheCurseOfTheSurvivalMigrant.GUI/Game.cs
+++ TheCurseOfTheSurvivalMigrant.GUI/Game.cs
@@ -6,17 +6,52 @@ namespace TheCurseOfTheSurvivalMigrant.GUI
     public partial class Game : UserControl
     {
         Controller _controller;
+        VerticalProgressBar _socialProgressBar;
+        VerticalProgressBar _lifeProgressBar;
+        VerticalProgressBar _moneyProgressBar;
         public Game(Controller c)
         {
             InitializeComponent();
             _controller = c;
             string test = _controller.Form.Context.CurrentEnigma.Question;
             EnigmaLabel.Text = _controller.Form.Context.CurrentEnigma.Question;
+            CreateVerticalProgressBar();
+        }
+
+        public void CreateVerticalProgressBar()
+        {
+            _socialProgressBar = new VerticalProgressBar();
+            panel1.Controls.Add(_socialProgressBar);
+            _socialProgressBar.Location = new System.Drawing.Point(205, 3);
+            _socialProgressBar.Size = new System.Drawing.Size(19, 72);
+            _lifeProgressBar = new VerticalProgressBar();
+            panel1.Controls.Add(_lifeProgressBar);
+            _lifeProgressBar.Location = new System.Drawing.Point(125, 3);
+            _lifeProgressBar.Size = new System.Drawing.Size(19, 72);
+            _moneyProgressBar = new VerticalProgressBar();
+            panel1.Controls.Add(_moneyProgressBar);
+            _moneyProgressBar.Location = new System.Drawing.Point(45, 3);
+            _moneyProgressBar.Size = new System.Drawing.Size(19, 72);
         }
 
         private void menuButton_Click(object sender, EventArgs e)
         {
             _controller.Form.ShowMenu();
+        }
+
+        private void panelAnswer1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panelAnswer2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelAnswer3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
